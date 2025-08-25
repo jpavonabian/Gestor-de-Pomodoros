7fbd6d41ac02e60b237cf611f83fc26023c4200d
Jesús Pavón Abián <galorasd@gmail.com>
2025-08-25 10:59:04 +0200

    Script

--- lanzar.bat
+++ lanzar.bat
@@ -1,17 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
-git add .
 
 REM Pedir número de versión
 set /p VERSION=Introduce el numero de version (ej: 1.2.3): 
 
 REM Hacer commit con el mensaje
-git commit -am "Release %VERSION%"
-if errorlevel 1 (
-    echo Error en el commit. Asegúrate de tener cambios preparados.
-    goto :eof
-)
-
 REM Crear el tag
 git tag %VERSION%
 if errorlevel 1 (
